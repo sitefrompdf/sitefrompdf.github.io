2586d470b2af39e73a5d8c1f64281b044a0a5c84
sitefrompdf <sitefrompdf@gmail.com>
2026-07-29 13:30:45 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'forms-portal-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'tour-ta.html',
+  'images/img1.jpeg',
+  'images/img2.jpeg',
+  'images/img3.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
